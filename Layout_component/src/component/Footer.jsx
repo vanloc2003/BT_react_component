a47a56d4751e968426e62bea36f271fbd4daf0e1
vanloc2003 <dangvanloc2003@gmail.com>
2025-03-02 05:38:16 +0700

    done layoutComponent2

--- Layout_component/src/component/Footer.jsx
+++ Layout_component/src/component/Footer.jsx
@@ -3,7 +3,7 @@
 const Footer = () => {
   return (
     <div>
-      <footer className="bg-white dark:bg-gray-900">
+      <footer className="bg-white mt-15 dark:bg-gray-900">
         <div className="mx-auto w-full max-w-screen-xl">
           <div className="grid grid-cols-2 gap-8 px-4 py-6 lg:py-8 md:grid-cols-4">
             <div>
